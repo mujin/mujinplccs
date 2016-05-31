3bf2ea086e20ee403c54262e14bb06847479219a
Ziyan Zhou <zhou@ziyan.info>
2016-05-31 00:01:13 -0700

    Mock up some API.

--- PLCServer.cs
+++ PLCServer.cs
@@ -9,5 +9,49 @@ namespace mujinplccs
 {
     public class PLCServer
     {
+        private string addr;
+        private bool isok = true;
+    
+        public PLCServer(string addr = "tcp://*:5555")
+        {
+            this.addr = addr;
+        }
+
+        /// <summary>
+        /// Start the PLC server on a background thread.
+        /// </summary>
+        public void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// Stop the PLC server. Will block until the background thread teminates.
+        /// </summary>
+        public void Stop()
+        {
+
+        }
+
+        public bool IsRunning()
+        {
+            return false;
+        }
+
+        private void ServerThread()
+        {
+            using (Context context = new Context())
+            using (Socket socket = context.Socket(SocketType.REP))
+            {
+                socket.Bind(this.addr);
+
+                while (this.isok)
+                {
+                    byte[] data = socket.Recv();
+
+                    socket.Send(data);
+                }
+            }
+        }
     }
 }
