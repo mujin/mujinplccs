e3fa1c9ad8f55d88e496c59f1ed34cf465a8b467
Ziyan Zhou <zhou@ziyan.info>
2016-06-06 01:56:02 -0700

    Add a C# example.

---
+++ samples/mujinplcexamplecs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mujinplccs;
+
+namespace mujinplcexamplecs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PLCServer server = new PLCServer("tcp://*:5555");
+            server.Start();
+
+            Console.WriteLine("Server started and listening on {0} ...", server.Address);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+
+            server.Stop();
+        }
+    }
+}
