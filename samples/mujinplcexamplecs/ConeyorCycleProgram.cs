2d311444dc43bbc4f6f142eb3f18f2e321a6d7d6
simon <simon.zheng@mujin.co.jp>
2018-11-22 15:53:36 +0900

    update Move function demo

--- samples/mujinplcexamplecs/ConeyorCycleProgram.cs
+++ samples/mujinplcexamplecs/ConeyorCycleProgram.cs
@@ -12,49 +12,69 @@ namespace mujinplcexamplecs
 {
     class Program
     {
-        // customer instantiate mujinsystem.
-        MujinSystem system = new MujinSystem();
-        // customer model the location as PLCLocation, set location info etc.
-        // maybe create a location mapping from name to index.
-        PLCLocation srclocaiton1 = new PLCLocation(locationIndex=1);
-        PLCLocation srclocaiton2 = new PLCLocation(locationIndex=2);
-        PLCLocation destlocation1 = new PLCLocation(locationIndex=3);
 
-        // customer add locations to system. 
-        system.AddLocation(srcLocation1);
-        system.AddLocation(srcLocation2);
-        system.AddLocation(srcLocation3);
+        public void Move(string expectedContainerId, string expectedContainerType){
 
-        // customer create order class, set orderinfo, such as parttype, picklocation, placelocation etc.
-        Order order1 = new Order("box1", orderPickLocation=srclocation1, orderPlaceLocation=destlocation3);
-        Order order2 = new Order("box2", orderPickLocation=srclocation2, orderPlaceLocation=destlocation3);
+            string nextContainerId = this.barcodeChecker.ScanContainerBarcode();
+            string nextContainerType = this.containerPool.GetContainerTypeById(nextContainerId);
 
-        // customer queue the order into corresponding location
-        system.AddOrderToLocationQueue(order1, srclocation1);
-        system.RemoveOrderFromLocationQueue(order2, srclocation2);
+            if(nextContainerId != expectedContainerId || nextContainerType != expectedContainerType){
+                // PANIC!
+                // customer need to deal with exception . cancel order etc..
+            }
+            else{
+                // ready..
+                while(!this.Ready()) // this == current location. maybe a subclass from PLCLocation base class.
+                {
+                    //....move check move check...
+                }
+                this.SetStatus(prohibited=False, containerId = nextContainerId, containerType=nextContainerType);
+            }
+        }
 
-        // MujinSystem finds queue isn't empty, start to deal with order in the queue
-        // ...
-        // After finish one order on locations
-        // Mujin will get an order from queue: nextOrder = GetNextOrder(location);
-        // Mujin will call location.Move(nextOrder.containerId, nextOrder.containerType);
-        // this step is for verification
+        static void Main(string[] args){
+            // customer instantiate mujinsystem.
+            MujinSystem system = new MujinSystem();
+            // customer model the location as PLCLocation, set location info etc.
+            // maybe create a location mapping from name to index.
+            PLCLocation srclocaiton1 = new PLCLocation(locationIndex=1, Move=Move);
+            PLCLocation srclocaiton2 = new PLCLocation(locationIndex=2, Move=Move);
+            PLCLocation destlocation1 = new PLCLocation(locationIndex=3, Move=Move);
+            // customer add locations to system. 
+            system.AddLocation(srcLocation1);
+            system.AddLocation(srcLocation2);
+            system.AddLocation(srcLocation3);
 
-        // .....
-        // customer move agv/conveyor to location, after it finished, customer will call
-        // location.SetStatus(prohibited=False, containerId='123', containerType='sometype');
+            // customer create order class, set orderinfo, such as parttype, picklocation, placelocation etc.
+            Order order1 = new Order("box1", orderPickLocation=srclocation1, orderPlaceLocation=destlocation3);
+            Order order2 = new Order("box2", orderPickLocation=srclocation2, orderPlaceLocation=destlocation3);
 
-        // Mujin will check if condition is satisifed and start to pickplace order.
+            // customer queue the order into corresponding location
+            system.AddOrderToLocationQueue(order1, srclocation1);
+            system.RemoveOrderFromLocationQueue(order2, srclocation2);
 
-        // Customer need to implement the 1. onFinishCodeChangedCallback and 2. onNumberPutInDestChangedCallback
-        // Mujin will invoke these method after corresponding signal and code changed to notify customer system.
+            // MujinSystem finds queue isn't empty, start to deal with order in the queue
+            // ...
+            // After finish one order on locations
+            // Mujin will get an order from queue: nextOrder = GetNextOrder(location);
+            // Mujin will call location.Move(nextOrder.containerId, nextOrder.containerType);
+            // this step is for verification
 
+            // .....
+            // customer move agv/conveyor to location, after it finished, customer will call
+            // location.SetStatus(prohibited=False, containerId='123', containerType='sometype');
 
-        // in summary, what customer need to do is
-        // 1. onFinishCodeChangedCallback (order info changed callback)
-        // 2. onNumberPutInDestChangedCallback (order info changed callback
-        // 3. SetStatus
-        // 4. Move
-        // 5. AddOrderToLocationQueue and RemoveOrderFromLocationQueue
+            // Mujin will check if condition is satisifed and start to pickplace order.
+            // Customer need to implement the 1. onFinishCodeChangedCallback and 2. onNumberPutInDestChangedCallback
+            // Mujin will invoke these method after corresponding signal and code changed to notify customer system.
+
+            // in summary, what customer need to do is
+            // 1. onFinishCodeChangedCallback (order info changed callback)
+            // 2. onNumberPutInDestChangedCallback (order info changed callback
+            // 3. SetStatus
+            // 4. Move
+            // 5. AddOrderToLocationQueue and RemoveOrderFromLocationQueue
+        }
+
     }
 }
